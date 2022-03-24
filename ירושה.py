b4d3ca7a360ab7b2a96df3768f4b174e01d783fb
aviva <avivaayasso@icloud.com>
2022-03-24 15:14:28 +0200

    class exercises...

---
+++ ירושה.py
@@ -0,0 +1,44 @@
+class Person:
+    def __init__(self,name,id):
+        self.__name=name
+        self.__id=id
+
+    def getName(self):
+        return self.__name
+
+    def setName(self,name):
+        self.__name=name
+
+    def getId(self):
+        return self.__id
+
+    def setId(self,id):
+        self.__id=id
+
+    def __str__(self):
+        return self.__name+" "+str(self.__id)
+
+    def makeVoice(self):
+        print("waseeeeweew")
+
+
+class Student(Person):
+
+    def __init__(self,name,id,grade):
+        super().__init__(name,id)
+        self.__grade=grade
+
+    def getGrade(self):
+        return self.__grade
+
+    def setGrade(self,grade):
+
+        self.__grade=grade
+
+    def __str__(self):
+        return super().__str__()+" "+self.__grade
+
+
+st1=Student("ron",12222,80)
+print(st1)
+per1=Person("david",11111)
